cd00484e410ba224fdea544371d774e7d843449e
Hideto Maguchi <mogeko6347@gmail.com>
2022-12-24 18:34:45 +0900

    Initial Commit

---
+++ SentimentAnalysis/SentimentAnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentimentAnalysisApp.swift
+//  SentimentAnalysis
+//
+//  Created by 間口秀人 on 2022/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SentimentAnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
